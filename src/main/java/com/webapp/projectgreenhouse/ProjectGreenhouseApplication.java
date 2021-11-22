3ca657dbd8dffd0d35e7abe564f98a6e1852182a
KriszTuri <turi.krisztina1999@gmail.com>
2021-11-22 10:22:46 +0100

    cloning last week's project

---
+++ src/main/java/com/webapp/projectgreenhouse/ProjectGreenhouseApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.projectgreenhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGreenhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectGreenhouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapp/projectgreenhouse/ProjectGreenhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.projectgreenhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGreenhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
